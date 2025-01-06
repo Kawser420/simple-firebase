09805bb029607071a8b6c7e7f216605b5b54d9c6
Kawser420 <muhammadkawser202@gmail.com>
2025-01-06 21:43:23 +0600

    just dote add

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Main from './layout/Main.jsx';
-import Home from './component/Home/Home.jsx';
-import Login from './component/Login/Login.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layout/Main.jsx";
+import Home from "./component/Home/Home.jsx";
+import Login from "./component/Login/Login.jsx";
 
 const router = createBrowserRouter([
   {
@@ -21,14 +18,14 @@ const router = createBrowserRouter([
       },
       {
         path: "/login",
-        element: <Login></Login>
-      }
-    ]
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
